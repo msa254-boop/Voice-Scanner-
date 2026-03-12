7933caa2c86460005bb927cbda626b3d752a7ab4
msa254-boop <msa254@njit.edu>
2026-03-12 15:08:53 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 let mic, fft;
-let smoothing = 0.8;
+let smoothing = 0.9;
+let waveSpeed = 0;
 
 function setup() {
   createCanvas(800, 400);
   noFill();
   strokeWeight(2);
-
   document.getElementById("activateMic").onclick = initMic;
 }
 
@@ -13,41 +13,44 @@ function initMic() {
   mic = new p5.AudioIn();
   mic.start();
 
-  fft = new p5.FFT(smoothing, 1024);
+  fft = new p5.FFT(smoothing, 512);
   fft.setInput(mic);
 
   document.getElementById("activateMic").disabled = true;
 }
 
 function draw() {
-  background(0);
+  background(0, 20); // trailing effect
 
   if (!mic) {
     fill(255);
     textAlign(CENTER);
-    text("Click 'Activate Mic' to start", width / 2, height / 2);
+    text("Click 'Activate Mic' to start", width/2, height/2);
     return;
   }
 
-  // GET WAVEFORM
+  waveSpeed += 2; // horizontal movement
+
+  let spectrum = fft.analyze();
   let waveform = fft.waveform();
 
-  // DRAW WAVEFORM LINE
-  stroke(0, 255, 200);
-  beginShape();
-  for (let i = 0; i < waveform.length; i++) {
-    let x = map(i, 0, waveform.length, 0, width);
-    let y = map(waveform[i], -1, 1, 0, height);
-    vertex(x, y);
-  }
-  endShape();
-
-  // OPTIONAL: DRAW FREQUENCY SPECTRUM BARS
-  let spectrum = fft.analyze();
-  for (let i = 0; i < spectrum.length; i += 5) {
-    let x = map(i, 0, spectrum.length, 0, width);
-    let h = map(spectrum[i], 0, 255, 0, height / 2);
-    stroke(255, 100, 150, 150);
-    line(x, height, x, height - h);
+  // Draw multiple layered waves
+  for (let layer = 0; layer < 3; layer++) {
+    beginShape();
+    stroke(
+      map(layer, 0, 2, 0, 255),
+      map(layer, 0, 2, 255, 100),
+      map(layer, 0, 2, 200, 255),
+      200
+    );
+    for (let i = 0; i < waveform.length; i++) {
+      let x = map(i, 0, waveform.length, 0, width);
+      let y =
+        height/2 +
+        waveform[i] * 200 * (layer + 1) +
+        sin((i + waveSpeed) * 0.05) * 30 * (layer + 1);
+      vertex(x, y);
+    }
+    endShape();
   }
 }
